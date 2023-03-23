27bda3325bc29f7c877f831f1a789c5e38b41ef1
Davidkuralan <thiruandshanker@gmail.com>
2023-03-23 13:32:24 +0530

    rendered list items

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let myLeads = [];
-let inputEl = document.getElementById('input-el')
-let inputBtn = document.getElementById('input-btn');
+let myLeads = ['www.cool.com','www.hot.com', 'www.mind.com'];
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
+
 
 inputBtn.addEventListener("click",() =>{
     myLeads.push(inputEl.value)
+    // myLeads.pop();
     console.log(myLeads);
 })
+
+for(let x = 0; x < myLeads.length; x++){
+    ulEl.innerHTML +=  "<li>" + myLeads[x] + "</li>"
+}
