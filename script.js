df053b7f96d6c8d82790bc54df66b0942862b180
Davidkuralan <thiruandshanker@gmail.com>
2023-03-23 14:06:47 +0530

    added anchor tag

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const ulEl = document.getElementById('ul-el');
 
 inputBtn.addEventListener("click",() =>{
     myLeads.push(inputEl.value);
+    inputEl.value = '';
     renderLeads();
 })
 
 function renderLeads(){
     let listItems = ""
 for(let x = 0; x < myLeads.length; x++){
-     listItems +=  "<li>" + myLeads[x] + "</li>"
+     listItems +=  "<li><a target = '_blank' href = '"+ myLeads[x] +"'>" + myLeads[x] + "</a></li>"
 //     const li = document.createElement('li')
 //     li.textContent = myLeads[x];
 //     ulEl.append(li);
